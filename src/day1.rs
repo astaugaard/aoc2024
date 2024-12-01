27573b67160f1395b424930ba5142f20657c7a15
astaugaard <64783657+astaugaard@users.noreply.github.com>
2024-12-01 12:03:45 -0500

    day1

---
+++ src/day1.rs
@@ -0,0 +1,84 @@
+use inttable::IntTable;
+use once_cell::sync::Lazy;
+
+use crate::{day, utils};
+
+type Input = Vec<(u32, u32)>;
+
+fn parser(input: String, _verbose: bool) -> Result<Input, String> {
+    Ok(input
+        .lines()
+        .map(|line| {
+            let mut nums = line
+                .split_whitespace()
+                .map(|num| num.parse::<u32>().unwrap());
+            (nums.next().unwrap(), nums.next().unwrap())
+        })
+        .collect::<Vec<_>>())
+}
+
+fn part_a(input: &Input) -> Option<String> {
+    let mut a = input.iter().map(|a| a.0).collect::<Vec<_>>();
+    let mut b = input.iter().map(|a| a.1).collect::<Vec<_>>();
+
+    radsort::sort(&mut a);
+    radsort::sort(&mut b);
+
+    let res = a
+        .into_iter()
+        .zip(b)
+        .map(|(a, b)| b.abs_diff(a))
+        .sum::<u32>();
+
+    Some(format!("{}", res))
+}
+
+fn part_b(input: &Input) -> Option<String> {
+    let mut times: IntTable<u32> = IntTable::with_capacity(1 << (input.len().ilog2() + 1));
+
+    for i in input.iter().map(|a| a.1) {
+        match times.get_mut(i as u64) {
+            Some(a) => {
+                *a += 1;
+            }
+            None => {
+                times.insert(i as u64, 1);
+            }
+        }
+    }
+
+    let res = input
+        .iter()
+        .map(|a| times.get(a.0 as u64).unwrap_or(&0) * a.0)
+        .sum::<u32>();
+
+    Some(format!("{}", res))
+}
+
+pub static DAY: Lazy<day::Day<Input>> = Lazy::new(|| day::Day {
+    // list of tests (functions that take a bool (weather to be verbose))
+    // and return an Option<String>
+    // None if there was no error
+    // Some(err) if the test failed
+    //
+    // a helper function golden is supplied so that you can test your parts against real inputs
+    //
+    // utils::golden(golden name, day, expected output part 1, expected output part 2)
+    //
+    // golden name is the name of the input file in the goldens directory
+    // the day is this structure so it is always &DAY
+    // the expected outputs are of the forms Some(expected) or None (when you are not testing that part with this golden)
+    //
+    // example for aoc day5 in 2021:
+    //
+    // use crate::utils;
+    //
+    // test: vec![utils::golden("day5", &DAY, Some("5"), Some("12"))],
+    tests: vec![utils::golden("day1", &DAY, Some("11"), Some("31"))],
+    ignore_failed_tests: true, // toggle to false to make the program exit if a test fails useful for debugging
+
+    // do not touch
+    parser: Box::new(parser),
+    part_a: Box::new(part_a),
+    part_b: Box::new(part_b),
+});
---
+++ src/main.rs
@@ -0,0 +1,174 @@
+use colored::Colorize;
+use std::{
+    fs::{self, File},
+    io::Write,
+    thread::available_parallelism,
+    time::Duration,
+};
+
+use chrono::{self, FixedOffset, NaiveDate, NaiveDateTime, TimeZone, Utc};
+use clap::{Parser, Subcommand};
+mod day;
+
+mod day1;
+mod day10;
+mod day11;
+mod day12;
+mod day13;
+mod day14;
+mod day15;
+mod day16;
+mod day17;
+mod day18;
+mod day19;
+mod day2;
+mod day20;
+mod day21;
+mod day22;
+mod day23;
+mod day24;
+mod day25;
+mod day3;
+mod day4;
+mod day5;
+mod day6;
+mod day7;
+mod day8;
+mod day9;
+mod utils;
+
+use day::{create_day, FETCH_CONFIG};
+use once_cell::sync::Lazy;
+
+#[derive(Parser)]
+#[command(version,about,long_about = None)]
+struct Cli {
+    #[arg(short, long, value_name = "VERBOSE")]
+    verbose: bool,
+
+    #[command(subcommand)]
+    command: Commands,
+
+    #[arg(short, long, value_name = "SKIP_TESTS")]
+    skip_tests: bool,
+}
+
+#[derive(Subcommand, Clone)]
+enum Commands {
+    Day {
+        #[arg(long, short)]
+        day: u32,
+    },
+    All,
+    SetFetchConfig {
+        #[arg(long, short)]
+        agent: String,
+
+        #[arg(long, short)]
+        oauthkey: String,
+
+        #[arg(long, short)]
+        year: u64,
+    },
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    let days: [Box<dyn Fn(bool, bool, u32) -> Result<(), String>>; 25] = [
+        create_day(Lazy::force(&day1::DAY)),
+        create_day(Lazy::force(&day2::DAY)),
+        create_day(Lazy::force(&day3::DAY)),
+        create_day(Lazy::force(&day4::DAY)),
+        create_day(Lazy::force(&day5::DAY)),
+        create_day(Lazy::force(&day6::DAY)),
+        create_day(Lazy::force(&day7::DAY)),
+        create_day(Lazy::force(&day8::DAY)),
+        create_day(Lazy::force(&day9::DAY)),
+        create_day(Lazy::force(&day10::DAY)),
+        create_day(Lazy::force(&day11::DAY)),
+        create_day(Lazy::force(&day12::DAY)),
+        create_day(Lazy::force(&day13::DAY)),
+        create_day(Lazy::force(&day14::DAY)),
+        create_day(Lazy::force(&day15::DAY)),
+        create_day(Lazy::force(&day16::DAY)),
+        create_day(Lazy::force(&day17::DAY)),
+        create_day(Lazy::force(&day18::DAY)),
+        create_day(Lazy::force(&day19::DAY)),
+        create_day(Lazy::force(&day20::DAY)),
+        create_day(Lazy::force(&day21::DAY)),
+        create_day(Lazy::force(&day22::DAY)),
+        create_day(Lazy::force(&day23::DAY)),
+        create_day(Lazy::force(&day24::DAY)),
+        create_day(Lazy::force(&day25::DAY)),
+    ];
+
+    // let default_parallelism_approx = available_parallelism().unwrap().get();
+
+    // println!("threads: {}", default_parallelism_approx);
+
+    match args.command {
+        Commands::Day { day } => {
+            match (*days[(day - 1) as usize])(args.verbose, args.skip_tests, day) {
+                Ok(()) => {}
+                Err(err) => {
+                    println!("{}", format!("error: {err}").red())
+                }
+            };
+        }
+        Commands::All => {
+            let current_time = Utc::now();
+            // let fetch_config = get_fetch_config();
+            for (day, dayfun) in days.iter().enumerate() {
+                let day = day+1;
+                let runday = match Lazy::force(&FETCH_CONFIG) {
+                    Some(conf) => {
+                        let release_time = FixedOffset::east_opt(5 * 60 * 60)
+                            .unwrap()
+                            .with_ymd_and_hms(
+                                conf.year as i32,
+                                12,
+                                day as u32,
+                                0, // midnight in the utc time zone
+                                0,
+                                2,
+                            );
+
+                        current_time.naive_utc() > release_time.unwrap().naive_utc()
+                    }
+                    None => true,
+                };
+
+                if runday {
+                    match (*dayfun)(args.verbose, args.skip_tests, day as u32) {
+                        Ok(()) => {}
+                        Err(err) => {
+                            println!("{}", format!("error: {err}").red())
+                        }
+                    }
+                }
+            }
+        }
+        Commands::SetFetchConfig {
+            agent,
+            oauthkey,
+            year,
+        } => {
+            let mut file = File::create("AOC_FETCH_CONFIG").unwrap();
+            file.write_all(format!("{agent}\n{oauthkey}\n{year}").as_bytes())
+                .unwrap();
+        }
+    }
+}
+
+
+// let client = reqwest::Client::new();
+//                     let res = match client
+//                         .get(&self.aoc_url)
+//                         .header("Cookie", format!("session={};", &self.aoc_token))
+//                         .send()
+//                         .await
+//                     {
+//                         Ok(x) => x.text().await,
+//                         Err(x) => Err(x),
+//                     };
