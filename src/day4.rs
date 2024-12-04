ddbf7e5d3a53dcff42bbe0d87cf58ff7bee7e267
Aaron Staugaard <astaugaard@icloud.com>
2024-12-04 07:24:24 -0500

    day4

--- src/day4.rs
+++ src/day4.rs
@@ -1,19 +1,80 @@
+use crate::day;
+use crate::utils;
+use itertools::Itertools;
 use once_cell::sync::Lazy;
 
-use crate::day;
+type Input = Vec<Vec<char>>;
 
-type Input = ();
-
-fn parser(_input: String, _verbose: bool) -> Result<Input, String> {
-    Ok(())
+fn parser(input: String, _verbose: bool) -> Result<Input, String> {
+    Ok(input
+        .lines()
+        .map(|line| line.chars().collect_vec())
+        .collect_vec())
 }
 
-fn part_a(_input: &Input) -> Option<String> {
-    None
+fn ismatch(i: i32, j: i32, di: i32, dj: i32, input: &[Vec<char>]) -> bool {
+    "XMAS".chars().enumerate().all(|(ind, c)| {
+        input
+            .get((i + (ind as i32) * di) as usize)
+            .map_or(None, |r| r.get((j + (ind as i32) * dj) as usize))
+            == Some(&c)
+    })
 }
 
-fn part_b(_input: &Input) -> Option<String> {
-    None
+fn countmatches(i: i32, j: i32, input: &[Vec<char>]) -> usize {
+    (-1..=1i32)
+        .flat_map(|di| (-1..=1i32).filter(move |dj| ismatch(i, j, di, *dj, &input)))
+        .count()
+}
+
+fn part_a(input: &Input) -> Option<String> {
+    let mut matches = 0;
+
+    for i in 0..input.len() {
+        for j in 0..input.len() {
+            matches += countmatches(i as i32, j as i32, input)
+        }
+    }
+
+    Some(matches.to_string())
+}
+
+fn ismatchx(i: usize, j: usize, input: &[Vec<char>]) -> bool {
+    if input[i][j] != 'A' {
+        return false;
+    }
+
+    if i == 0 || j == 0 || i == input.len() - 1 || j == input.len() - 1 {
+        return false;
+    }
+
+    if !((input[i - 1][j - 1] == 'M' && 'S' == input[i + 1][j + 1])
+        || (input[i - 1][j - 1] == 'S' && 'M' == input[i + 1][j + 1]))
+    {
+        return false;
+    }
+
+    if !((input[i + 1][j - 1] == 'M' && 'S' == input[i - 1][j + 1])
+        || (input[i + 1][j - 1] == 'S' && 'M' == input[i - 1][j + 1]))
+    {
+        return false;
+    }
+
+    return true;
+}
+
+fn part_b(input: &Input) -> Option<String> {
+    let mut matches = 0;
+
+    for i in 0..input.len() {
+        for j in 0..input.len() {
+            if ismatchx(i, j, input) {
+                matches += 1;
+            }
+        }
+    }
+
+    Some(matches.to_string())
 }
 
 pub static DAY: Lazy<day::Day<Input>> = Lazy::new(|| day::Day {
@@ -22,3 +83,19 @@ pub static DAY: Lazy<day::Day<Input>> = Lazy::new(|| day::Day {
     part_a: Box::new(part_a),
     part_b: Box::new(part_b),
 });
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn goldens() {
+        utils::golden("day4", &DAY, Some("18"), Some("9"), false)
+    }
+
+    // #[test]
+    // fn finalanswer() {
+    //     utils::finalanswer(1, &DAY, Some("2057374"), Some("23177084"), false);
+
+    // }
+}
